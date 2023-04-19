52aaddb68d04bc151577cb5036a355172d08c83e
Santhil Kalantre <santhil@Santhils-MacBook-Air.local>
2023-04-19 16:47:07 +0530

    Added Mail Service to inform doctor

--- src/main/java/com/example/noidea2/controller/auth/AuthController.java
+++ src/main/java/com/example/noidea2/controller/auth/AuthController.java
@@ -14,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,12 +27,23 @@ import java.util.Map;
 @CrossOrigin
 public class AuthController {
     @Autowired
+    private JavaMailSender javaMailSender;
+    @Autowired
     private JwtUtil jwtUtil;
 
     @Autowired
     private CredsRepo credsRepo;
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        javaMailSender.send(message);
+    }
 
     @GetMapping("/try/something")
     public ResponseEntity<String> welcome(@RequestHeader("Authorization") String token) throws Exception{
@@ -65,6 +79,8 @@ public class AuthController {
         String uname=jwtUtil.extractUsername(token);
         Creds c=credsRepo.findByUsername(uname);
         return new DocReturnObject(token , c.getId());
+
+
     }
 
     @PostMapping("/doc/register")
@@ -75,6 +91,32 @@ public class AuthController {
         if(c.getRole()!=0) throw new Exception("Chal na admin ko bhej");
         try{
             Creds ct = credsRepo.save(registerRequest);
+            sendEmail("santhilkalantre@gmail.com",
+                    "Your Credentials for Accessing the Medical Records System",
+                    "Dear Doctor,\n" +
+                            "\n" +
+                            "I am writing to provide you with your login credentials for accessing our medical records system. As an authorized user, you will be able to access patient medical records and update them as needed.\n" +
+                            "\n" +
+                            "Your login credentials are as follows:\n" +
+                            "\n" +
+                            "Username: "+registerRequest.getUsername()+"\n" +
+                            "Password: "+ registerRequest.getPassword() +"\n" +
+                            "\n" +
+                            "Please note that the password provided is case sensitive and must be kept confidential to ensure the security of patient data.\n" +
+                            "\n" +
+                            "To access the system, please visit [Insert Website URL] and enter your login credentials. If you experience any difficulties or have questions, please do not hesitate to contact our IT support team at [Insert Contact Information].\n" +
+                            "\n" +
+                            "Thank you for joining our team, and we look forward to working with you.\n" +
+                            "\n" +
+                            "Best regards,\n" +
+                            "\n" +
+                            "Admin\n" +
+                            "admin@admin.com\n" +
+                            "+91-10110101\n" +
+                            "\n" +
+                            "\n" +
+                            "\n" +
+                            "\n");
             return ct.getId();
         }catch (Exception ex){
 //            System.out.println(ex);
@@ -114,3 +156,5 @@ class DocReturnObject{
     private String jwttoken;
     private Integer did;
 }
+
+
